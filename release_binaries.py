f7c452b6315a6286ced91484cc3c0361609048f7
imirzov <imirzov@gmail.com>
2019-08-19 13:43:22 +0200

    release_binaries updated

--- release_binaries.py
+++ release_binaries.py
@@ -4,41 +4,48 @@
     © Ihor Mirzov, August 2019
     Distributed under GNU General Public License v3.0
 
-    Prepare binaries for publishing.
+    Prepare CCX_CAE binaries for publishing.
 """
 
 
-import os, shutil, datetime
+import os, shutil, datetime, subprocess
 import PyInstaller.__main__
 
 
 if __name__ == '__main__':
-
-    DIRECTORY = './dist/ccx_cae/'
-    DATE = datetime.datetime.now().strftime('%Y%m%d')
-    op_sys = os.system('windows' if os.name=='nt' else 'ubuntu')
-    ARCH = 'ccx_cae_' + op_sys + '_' + DATE
-    TEMP = os.system('C:\Windows\Temp\' if os.name=='nt' else '/tmp/')
+    PROJECT_NAME = 'ccx_cae'
+    DIRECTORY = './dist/' + PROJECT_NAME + '/'
+    DATE = '_' + datetime.datetime.now().strftime('%Y%m%d')
+    op_sys = '_windows' if os.name=='nt' else '_ubuntu'
+    ARCH = '../' + PROJECT_NAME + op_sys + DATE
+    TEMP = 'C:\\Windows\\Temp\\' if os.name=='nt' else '/tmp/'
 
     # Run pyinstaller to create binaries
-    PyInstaller.__main__.run(['ccx_cae.py', '--workpath=' + TEMP])
-
-    # Copy some files and folders from sources
-    copy_files = ['doc', 'icons', 'examples',
-                'ccx_cae.ui', 'ccx_dialog.ui',
-                'ccx_dom.inp', 'ccx_mesh.inp', 'Materials.inp',
-                'Settings.env']
-    for f in copy_files:
-        os.system('cp -r ' + f + ' ' + DIRECTORY + f)
-
-    # Make archive
-    os.system('rm ' + ARCH + '.zip') # delete old
-    shutil.make_archive(ARCH, 'zip', DIRECTORY) # create new
-
-    # Remove unneeded files and folders
-    os.system('rm -r ' + TEMP + 'ccx_cae ./dist')
-    os.system('rm ccx_cae.spec')
+    PyInstaller.__main__.run([PROJECT_NAME + '.py', '--workpath=' + TEMP])
 
     # Delete cached files
     if os.path.isdir('__pycache__'):
         shutil.rmtree('__pycache__') # works in Linux as in Windows
+
+    # Delete .spec file
+    if os.path.isfile(PROJECT_NAME + '.spec'):
+        subprocess.run('rm ' + PROJECT_NAME + '.spec', shell=True)
+
+    # Copy some files and folders from sources
+    copy_files = []
+    skip_files = ['dist', '.git']
+    for file_name in os.listdir():
+        # Append all dirs and files except Python sources
+        if not file_name in skip_files and \
+           not file_name.endswith('.py'):
+            copy_files.append(file_name)
+    for f in copy_files:
+        subprocess.run('cp -r ' + f + ' ' + DIRECTORY + f, shell=True)
+
+    # Make archive
+    if os.path.isfile(ARCH + '.zip'):
+        subprocess.run('rm ' + ARCH + '.zip', shell=True) # delete old
+    shutil.make_archive(ARCH, 'zip', DIRECTORY) # create new
+
+    # Remove unneeded files and folders
+    subprocess.run('rm -r ' + TEMP + PROJECT_NAME + ' ./dist', shell=True)
