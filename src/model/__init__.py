4cfd95c300834daaafe8e4a4f38931d27644cf14
imirzov <imirzov@gmail.com>
2020-04-26 20:47:43 +0200

    Optimized model package

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+""" © Ihor Mirzov, December 2019
+Distributed under GNU General Public License v3.0
+
+Model unites KOM object and parsers.
+See scheme on architecture.odp. """
+
 from . import parsers
 from . import job
 from . import kom
-from . import model
+
+import logging, os
+
+class Model:
+
+    def __init__(self):
+
+        # Empty KOM w/o implementations
+        self.KOM = kom.KOM()
+
+        # Variable names below should exactly represent KOM group names
+        self.Mesh = None
+        self.Interactions = None
+        self.Constraints = None
