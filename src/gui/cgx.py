0a2e2e3a9ebe1e45f0e142dd6c28e26c3dfed247
imirzov <imirzov@gmail.com>
2020-05-02 14:54:34 +0200

    Better CGX process killer

--- src/gui/cgx.py
+++ src/gui/cgx.py
@@ -8,6 +8,8 @@ Distributed under GNU General Public License v3.0
 Methods for CGX window. """
 
 import os
+import time
+import logging
 try:
     import psutil
 except:
@@ -15,14 +17,34 @@ except:
         + 'pip3 install psutil'
     sys.exit(msg)
 
-def kill(parent=None):
-    if parent is None:
-        parent = psutil.Process()
-    for ch in parent.children():
-        kill(parent=ch)
-        ch.terminate()
-        if not ch.pid in psutil.pids():
-            print('Killed PID', ch.pid)
+# Kill all CGX processes
+def kill():
+    for p in psutil.process_iter():
+        if p.name() in ['cgx', 'cgx.exe']:
+            pid = p.pid
+            count = 1
+            while psutil.pid_exists(pid):
+                try:
+                    p.kill()
+                except:
+                    pass
+                time.sleep(0.1)
+                count += 1
+                if count > 10:
+                    logging.error('Can not kill CGX, PID={}.'.format(pid))
+                    break
+            if not psutil.pid_exists(pid): 
+                logging.info('Killed PID={}.'.format(pid))
+
+# Kill child CGX processes
+# def kill(parent=None):
+#     if parent is None:
+#         parent = psutil.Process()
+#     for ch in parent.children(recursive=True):
+#         pid = ch.pid
+#         ch.terminate()
+#         if not pid in psutil.pids():
+#             print('Killed PID', pid)
 
 def paint_elsets(w, elsets):
     colors = 'rgbymntk'
