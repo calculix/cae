3170ae56462e410c8985d6e20b806e9b05254319
imirzov <imirzov@gmail.com>
2020-06-13 08:52:56 +0200

    Comments on how to call added

--- cae.sh
+++ cae.sh
@@ -4,9 +4,9 @@ DIR="$(dirname $(readlink -f $0))"
 
 if [ -z "$1" ]
 then
+    # Calling ./cae.sh
     exec "$DIR/src/cae"
 else
-    # ./cae.sh -inp default.inp
-    # ./cae.sh -inp=default.inp
+    # Calling ./cae.sh -inp default.inp
     exec "$DIR/src/cae $1 $2"
 fi
