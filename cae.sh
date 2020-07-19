ab1ebdd206beec7e248c7d9f9f49c8f4275c07fd
imirzov <imirzov@gmail.com>
2020-07-19 21:52:06 +0200

    More elegant cae.sh

--- cae.sh
+++ cae.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-DIR="$(dirname $(readlink -f $0))"
-CAE="$DIR/src/cae"
+DIR=$(dirname $(readlink -f $0))
+CAE=$DIR/src/cae
 
-if [ -f "$CAE" ]; then
+if [ -f $CAE ]; then
     echo "Running binary."
 else
     echo "Binary does not exist. Running source code."
-    CAE="$CAE.py"
+    CAE=$CAE.py
 fi
 
-if [ -z "$1" ]; then
-    exec "$CAE"
-else
-    # Arguments: -inp default.inp
-    exec "$CAE $1 $2"
-fi
+exec $CAE $*
