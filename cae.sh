a8ca6d3820fa4809facc0b1ba19f4650c66ea2da
imirzov <imirzov@gmail.com>
2020-07-19 20:13:48 +0200

    Allow to run source code via cae.sh

--- cae.sh
+++ cae.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 DIR="$(dirname $(readlink -f $0))"
+CAE="$DIR/src/cae"
 
-if [ -z "$1" ]
-then
-    # Calling ./cae.sh
-    exec "$DIR/src/cae"
+if [ -f "$CAE" ]; then
+    echo "Running binary."
 else
-    # Calling ./cae.sh -inp default.inp
-    exec "$DIR/src/cae $1 $2"
+    echo "Binary does not exist. Running source code."
+    CAE="$CAE.py"
+fi
+
+if [ -z "$1" ]; then
+    exec "$CAE"
+else
+    # Arguments: -inp default.inp
+    exec "$CAE $1 $2"
 fi
