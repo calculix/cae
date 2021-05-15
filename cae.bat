3fff9e97bcb7aa96dcde6cb89bdc74eb4e0264dc
imirzov <imirzov@gmail.com>
2021-05-15 22:51:22 +0200

    Updated shortcuts to run the source code

--- cae.bat
+++ cae.bat
@@ -1,13 +1,2 @@
 @echo off
-
-set DIR=%~dp0%
-set CAE=%DIR%src\cae.exe
-
-if exist %CAE% (
-    echo Running binary.
-    start "" %CAE% %*
-) else (
-    echo Binary does not exist. Running source code.
-    set CAE=%DIR%src\cae.py
-    start "" pythonw %CAE% %*
-)
+start "" pythonw %~dp0%src\cae.py %*
--- cae.sh
+++ cae.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-DIR=$(dirname $(readlink -f $0))
-CAE=$DIR/src/cae
-
-if [ -f $CAE ]; then
-    echo "Running binary."
-else
-    echo "Binary does not exist. Running source code."
-    CAE=$CAE.py
-fi
-
-exec $CAE $*
+exec $(dirname $(readlink -f $0))/src/cae.py $*
