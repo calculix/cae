784327809a10e5c7f72820a6e6624902a93fa0c6
imirzov <imirzov@gmail.com>
2021-05-15 22:49:00 +0200

    Major refactor of window.py

--- src/gui/window.py
+++ src/gui/window.py
@@ -41,77 +41,76 @@ except:
     sys_path = os.path.join(sys_path, '..')
     sys_path = os.path.normpath(sys_path)
     sys_path = os.path.realpath(sys_path)
-    sys.path.insert(0, sys_path)
+    if sys_path not in sys.path:
+        sys.path.insert(0, sys_path)
     import clean
     import gui
     import gui.connection
     import path
     import settings
 
-# Common wrapper for MasterWindow/SlaveWindow run() method
-def run_wrapper():
+# Common wrapper for MasterWindow/SlaveWindow __init__() method
+def init_wrapper():
     def wrap(method):
-        def fcn(self):
+        def fcn(self, arg):
             before = get_opened_windows()
 
-            method(self)
+            method(self, arg)
 
             # Wait for window to open ang get its info
-            new_window = []
+            new_windows_infos = []
             start = time.perf_counter() # start time
-            while not len(new_window):
+            while not len(new_windows_infos):
                 after = get_opened_windows()
                 time.sleep(0.3)
-                new_window = get_new_window(before, after)
+                new_windows_infos = get_new_windows_infos(before, after)
                 if time.perf_counter() - start > 3:
                     msg = 'New slave window starts too slowly.'
                     logging.error(msg)
                     break
 
-            if len(new_window):
-                wi = new_window[0]
+            if len(new_windows_infos):
+                wi = new_windows_infos[0]
+                try:
+                    wi.pid = self.process.pid
+                except:
+                    pass # MasterWindow has no process variable
                 msg = wi.to_string()
-                logging.info(msg)
+                logging.debug(msg)
                 self.info = wi
-                return wi
-            else:
-                return None
 
         return fcn
     return wrap
-    
+
 
 """ Window factory class - used to create master and slave windows
 and bind/connect therm together. Logging facility as killing methods
 for slave window are also maintained here. """
 class Factory:
 
-    def __init__(self, s, master_xml):
+    def __init__(self, s):
         self.s = s # global settings
-        self.mw = MasterWindow(master_xml)
+        self.mw = None # master window
         self.sw = None # slave window
         self.stdout_readers = [] # for slave window
-        wc1 = gui.connection.WindowConnection(None)
-        wc2 = gui.connection.WindowConnection(None)
-        self.connections = {1:wc1, 2:wc2}
-        self.w = QtWidgets.QDesktopWidget().availableGeometry().width()
-        self.h = QtWidgets.QDesktopWidget().availableGeometry().height()
+        args = [None, None]
+        self.connections = {
+            1:gui.connection.WindowConnection(*args), # Main Window + CGX
+            2:gui.connection.WindowConnection(*args), # Dialog + web browser
+            }
 
         # Caller fuction name: cgx.open_inp | cgx.open_frd | other
         self.mode = None
 
+    def run_master(self, xml):
+        self.mw = MasterWindow(xml)
+
     # Run slave process
     # Close opened CGX (if any) and open a new one
     # Get window ID, align windows and post to CGX
-    def run_slave(self, cmd, title):
+    def run_slave(self, cmd):
         self.kill_slave()
-        self.sw = SlaveWindow(cmd, title)
-        self.sw.run()
-        # if self.sw.info is not None:
-        #     print(self.sw.info.to_string())
-
-        # Create connection between master and slave windows
-        self.create_connection(1)
+        self.sw = SlaveWindow(cmd)
 
         # Start stdout reading and logging thread
         self.start_stdout_reader('read_cgx_stdout')
@@ -128,7 +127,7 @@ class Factory:
 
         self.stop_stdout_readers()
 
-        self.connections[1].wid2 = None
+        # self.connections[1].wid2 = None
         count = 0
         while self.sw.process.poll() is None:
             try:
@@ -144,18 +143,18 @@ class Factory:
                 break
         if self.sw.process.poll() is None:
             msg = 'Can not kill {}, PID={}.'\
-                .format(self.sw.title, self.sw.process.pid)
+                .format(self.sw.info.wname, self.sw.process.pid)
             logging.warning(msg)
         else:
             msg = 'Killed {}, PID={}.'\
-                .format(self.sw.title, self.sw.process.pid)
+                .format(self.sw.info.wname, self.sw.process.pid)
             logging.debug(msg)
             self.sw.process = None
+            self.sw.info = None
 
     # Start stdout reading and logging thread
     def start_stdout_reader(self, aim):
-        sr = gui.log.CgxStdoutReader(self.sw.process.stdout,
-            aim, self)
+        sr = gui.log.CgxStdoutReader(self.sw.process.stdout, aim, self)
         self.stdout_readers.append(sr)
         sr.start()
 
@@ -172,86 +171,105 @@ class Factory:
 
     # Connect master and slave windows and align them
     def create_connection(self, _id):
+        if self.mw is None:
+            logging.error('No master window.')
+            return
+        if self.sw is None:
+            logging.error('No slave window.')
+            return
+
+        args = [self.mw, self.sw]
         if os.name == 'nt':
-            wc = gui.connection.WindowConnectionWindows(self)
-        elif os.name == 'posix':
-            wc = gui.connection.WindowConnectionLinux(self)
+            wc = gui.connection.WindowConnectionWindows(*args)
         else:
-            msg = 'Sorry, {} OS is not supported.'.format(os.name)
-            raise SystemExit(msg) # the best way to exit
+            wc = gui.connection.WindowConnectionLinux(*args)
+
         self.connections[_id] = wc
-        wc.connect()
         if self.s.align_windows:
             wc.align()
         return wc
 
 
-# Left application's window - CAE
+# Left window - main window of the app
 class MasterWindow(QtWidgets.QMainWindow):
 
+    # Load form and show the window
+    @init_wrapper()
     def __init__(self, xml):
-        self.xml = xml
         self.info = None
 
-    # Load for and show the window
-    @run_wrapper()
-    def run(self):
-        QtWidgets.QMainWindow.__init__(self) # create main window
-        uic.loadUi(self.xml, self) # load form
+        # QtWidgets.QMainWindow.__init__(self) # create main window
+        super(MasterWindow, self).__init__()
+        uic.loadUi(xml, self) # load form
 
         # Handler to show logs in the CAE's textEdit
         if hasattr(self, 'textEdit'): # skip for test_sendkeys
             gui.log.add_text_handler(self.textEdit)
-        
+
         self.show()
 
     # Open links from the Help menu
     def help(self, url):
-        logging.info('Going to\n' + url)
-        if not webbrowser.open(url, new=2):
-            logging.warning('Can\'t open url: ' + url)
+        if webbrowser.open(url, new=2):
+            logging.info('Going to\n' + url)
+        else:
+            logging.warning('Can not open url\n' + url)
 
 
-# Right application's window - CGX, text editor, web browser
+""" Right application's window - CGX, text editor,
+web browser. Starts as a separate process """
 class SlaveWindow:
 
-    # Run command - open new slave window - text editor or CGX
-    def __init__(self, cmd, title):
-        # TODO Use WindowInfo everywhere
-        self.cmd = cmd
-        self.title = title
-        self.info = None
-
-        if os.name == 'nt':
-            self.shell = True
-        elif os.name == 'posix':
-            self.shell = False
-        else:
-            msg = 'Sorry, {} OS is not supported.'.format(os.name)
-            raise SystemExit(msg) # the best way to exit
-
-    # Run slave process without terminal/cmd window
-    @run_wrapper()
-    def run(self):
-        self.process = subprocess.Popen(self.cmd.split(),
+    # Run slave process without terminal window, set window info
+    @init_wrapper()
+    def __init__(self, cmd):
+        self.info = None # WindowInfo will be set in @init_wrapper
+        shell = (os.name == 'nt') # True or False
+        self.process = subprocess.Popen(cmd.split(),
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            shell=self.shell)
+            shell=shell)
 
+
+# Snapshot window list
+def get_opened_windows():
+    args = [None, None]
+    if os.name == 'posix':
+        wc = gui.connection.WindowConnectionLinux(*args)
+    elif os.name == 'nt':
+        wc = gui.connection.WindowConnectionWindows(*args)
+    else:
+        logging.error('Unsupported OS.')
+        raise SystemExit
+    return wc.get_opened_windows()
+
+# Gets two dictionaries with opened windows,
+# compares and returns info for newly opened window.
+# Only one new window is allowed.
+def get_new_windows_infos(opened_windows_before, opened_windows_after):
+    new_windows_infos = []
+    for wi in opened_windows_after:
+        if wi.wid not in [i.wid for i in opened_windows_before]:
+            new_windows_infos.append(wi)
+    if len(new_windows_infos) > 1:
+        msg = 'Can\'t define slave WID: there is more than one newly opened window.'
+        logging.error(msg)
+        raise SystemExit
+    return new_windows_infos
 
 # Keycodes sending to text editor and CGX
 def test_sendkeys():
     p = path.Path()
     s = settings.Settings(p)
-    master_xml = os.path.join(p.config, 'sendkeys.xml')
+    xml = os.path.join(p.config, 'sendkeys.xml')
 
     # Create application
     app = QtWidgets.QApplication([])
 
     # Create master window
-    f = Factory(s, master_xml)
-    f.mw.run()
+    f = Factory(s)
+    f.run_master(xml)
 
     # Configure global logging level
     logging.getLogger().setLevel(s.logging_level)
@@ -261,19 +279,20 @@ def test_sendkeys():
     # Map methods to GUI buttons
     if os.name == 'nt':
         cmd1 = 'notepad.exe'
-        title1 = 'Untitled - Notepad'
+        # title1 = 'Untitled - Notepad'
     else:
         cmd1 = 'gedit'
-        title1 = 'Untitled Document 1 - gedit'
+        # title1 = 'Untitled Document 1 - gedit'
         if which(cmd1) is None:
             cmd1 = 'kate'
-            title1 = 'Untitled Document 1 - Kate'
+            # title1 = 'Untitled Document 1 - Kate'
             if which(cmd1) is None:
                 cmd1 = ''
                 msg = 'Neither gedit nor kate is available!'
                 logging.error(msg)
                 f.mw.b0.setDisabled(True)
-    f.mw.b0.clicked.connect(lambda: f.run_slave(cmd1, title1))
+    f.mw.b0.clicked.connect(lambda: f.run_slave(cmd1))
+    f.mw.b0.clicked.connect(lambda: f.create_connection(1))
     f.mw.b1.clicked.connect(lambda: gui.cgx.open_inp(p, f, s.start_model, 1))
     f.mw.b2.clicked.connect(lambda: f.connections[1].post('plot n all'))
     f.mw.b3.clicked.connect(lambda: f.connections[1].post('plot e all'))
@@ -288,40 +307,9 @@ def test_sendkeys():
     a = app.exec()
     f.kill_slave()
 
-# Snapshot window list
-def get_opened_windows():
-    if os.name == 'posix':
-        wc = gui.connection.WindowConnectionLinux(None)
-    elif os.name == 'nt':
-        wc = gui.connection.WindowConnectionWindows(None)
-    else:
-        logging.error('Unsupported OS.')
-        raise SystemExit
-    return wc.get_opened_windows()
-
-# Gets two dictionaries with opened windows,
-# compares and returns info for newly opened window.
-# Only one new window is allowed.
-def get_new_window(opened_windows_before, opened_windows_after):
-    new_window = []
-    for wi in opened_windows_after:
-        if wi.wid not in [i.wid for i in opened_windows_before]:
-            new_window.append(wi)
-    if len(new_window) > 1:
-        msg = 'Can\'t define slave WID: there is more than one newly opened window.'
-        logging.error(msg)
-        raise SystemExit
-    return new_window
-
 # Run test
 if __name__ == '__main__':
     logging.basicConfig(level=logging.NOTSET, format='%(message)s')
     clean.screen()
-
     test_sendkeys()
-
-    # p = path.Path()
-    # url = 'file://' + p.doc + '/NODE.html'
-    # run_web_browser(url)
-
     clean.cache()
