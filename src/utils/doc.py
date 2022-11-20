5d477b910b3a36345d314326b1b1953b0d04d844
Ihor Mirzov <imirzov@gmail.com>
2022-11-20 18:53:44 +0100

    Documentation update to 2.20

--- src/utils/doc.py
+++ src/utils/doc.py
@@ -12,8 +12,16 @@ then run prepare_documentation() method.
 # Standard modules
 import os
 import re
+import sys
 
 # My modules
+sys_path = os.path.abspath(__file__)
+sys_path = os.path.dirname(sys_path)
+sys_path = os.path.join(sys_path, '..')
+sys_path = os.path.normpath(sys_path)
+sys_path = os.path.realpath(sys_path)
+if sys_path not in sys.path:
+    sys.path.insert(0, sys_path)
 from path import p
 
 
