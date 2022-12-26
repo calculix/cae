c01aa3a14552731b0f3952f6a03481f8f922551e
Ihor Mirzov <imirzov@gmail.com>
2022-12-26 20:49:48 +0100

    Full support of *CONTACT OUTPUT

--- src/gui/dialog.py
+++ src/gui/dialog.py
@@ -525,7 +525,7 @@ def test_dialog():
 
     """Create keyword dialog."""
     app = QtWidgets.QApplication(sys.argv)
-    item = KWL.get_keyword_by_name('*CONTACT FILE')
+    item = KWL.get_keyword_by_name('*CONTACT OUTPUT')
     from gui.window import df
     df.run_master_dialog(item) # 0 = cancel, 1 = ok
 
