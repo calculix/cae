e432401f4338cdce943e40a172f593f6b978ba00
Ihor Mirzov <imirzov@gmail.com>
2022-12-28 18:32:44 +0100

    Full support of *DENSITY

--- src/gui/dialog.py
+++ src/gui/dialog.py
@@ -646,7 +646,7 @@ def test_dialog():
 
     """Create keyword dialog."""
     app = QtWidgets.QApplication(sys.argv)
-    item = KWL.get_keyword_by_name('*DEFORMATION PLASTICITY')
+    item = KWL.get_keyword_by_name('*DENSITY')
     from gui.window import df
     df.run_master_dialog(item) # 0 = cancel, 1 = ok
 
