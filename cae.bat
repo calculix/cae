4467b990269da5fcbf29c226735651f0bbfcb2b8
imirzov <imirzov@gmail.com>
2023-01-06 18:35:06 +0100

    Windows .bat-file: do not autoclose CMD window

--- cae.bat
+++ cae.bat
@@ -1,4 +1,4 @@
 @echo off
 python -V | findstr "3."
-if %ERRORLEVEL%==0 python %~dp0%src\cae.py
-else echo "Python 3 needed."
+if %ERRORLEVEL%==0 (python %~dp0%src\cae.py) else echo "Python 3 needed."
+pause >nul
