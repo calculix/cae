d0c50ee8c586fa0c59070ce6d7c369f6fad172d5
imirzov <imirzov@gmail.com>
2023-01-06 19:05:58 +0100

    Windows .bat-file: try/catch python exists

--- cae.bat
+++ cae.bat
@@ -1,4 +1,12 @@
 @echo off
-python -V | findstr "3."
-if %ERRORLEVEL%==0 (python %~dp0%src\cae.py) else echo "Python 3 needed."
+
+python %~dp0%src\cae.py
+if %ERRORLEVEL% neq 0 goto catch
 pause >nul
+exit /b 0
+
+:catch
+echo Command 'python' is needed to run CAE.
+echo Please, install Python v3.
+pause >nul
+exit /b 1
