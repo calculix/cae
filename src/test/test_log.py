bb5d251ae4dd68fd27f48cf39b93679eb99ece52
Ihor Mirzov <imirzov@gmail.com>
2023-01-16 22:09:43 +0100

    Unittests: refactored logging test

--- src/test/test_log.py
+++ src/test/test_log.py
@@ -14,7 +14,7 @@ import logging
 import unittest
 
 # External modules
-from PyQt5 import QtWidgets
+# from PyQt5 import QtWidgets
 
 # My modules
 sys_path = os.path.abspath(__file__)
@@ -24,42 +24,47 @@ sys_path = os.path.normpath(sys_path)
 sys_path = os.path.realpath(sys_path)
 if sys_path not in sys.path:
     sys.path.insert(0, sys_path)
-from gui.window import MasterWindow
+# from gui.window import MasterWindow
 import log
 
 
-app = QtWidgets.QApplication([])
-mw = MasterWindow()
+# app = QtWidgets.QApplication([])
+# mw = MasterWindow()
 # log.get_logging_info()
 
-logging.debug('debug')
-logging.info('info')
-logging.warning('warning')
-logging.error('error')
 
 # txt = mw.textEdit.toPlainText()
 # app.exec()
 
 
-# class Test(unittest.TestCase):
-#     """Test if logging works."""
+class TestLog(unittest.TestCase):
 
-#     def test1_debug(self):
-#         global txt
-#         self.assertTrue('debug' in txt)
+    # def test1_debug(self):
+    #     global txt
+    #     self.assertTrue('debug' in txt)
 
-#     def test2_info(self):
-#         global txt
-#         self.assertTrue('info' in txt)
+    # def test2_info(self):
+    #     global txt
+    #     self.assertTrue('info' in txt)
 
-#     def test3_warning(self):
-#         global txt
-#         self.assertTrue('warning' in txt)
+    # def test3_warning(self):
+    #     global txt
+    #     self.assertTrue('warning' in txt)
 
-#     def test4_error(self):
-#         global txt
-#         self.assertTrue('error' in txt)
+    # def test4_error(self):
+    #     global txt
+    #     self.assertTrue('error' in txt)
+
+    def test_log(self):
+        """Test if logging works"""
+        print()
+        log.add_stream_handler(level=logging.DEBUG)
+        logging.debug('debug')
+        logging.info('info')
+        logging.warning('warning')
+        logging.error('error')
+        self.assertTrue(True)
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
