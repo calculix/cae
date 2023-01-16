602f58d9f6322380c201dfb52f5c04a842fcd2d2
Ihor Mirzov <imirzov@gmail.com>
2023-01-16 19:15:58 +0100

    Local python for tests

--- tests.sh
+++ tests.sh
@@ -6,9 +6,10 @@ cd $SCRIPT_DIR/src/test
 for f in $(find . -name \*.py)
 do
     f="${f##*/}"
-    python3 -m unittest -v $f
+    $SCRIPT_DIR/bin/python/bin/python3 -m unittest -v $f
     echo ''
     echo ''
     echo ''
 done
 cd $INITIAL_DIR
+read
